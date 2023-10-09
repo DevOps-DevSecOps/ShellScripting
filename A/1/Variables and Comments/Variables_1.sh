65c4338434daa7d64212a36c9983867498b169d2
aruntheja2 <aruntheja9@outlook.in>
2023-10-09 03:18:58 +0000

    Variables_1.sh

---
+++ A/1/Variables and Comments/Variables_1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo $BASH
+echo $BASH_VERSION
+echo $HOME
+echo $PWD
+
+echo Shell name is $BASH
+echo "Version will be $BASH_VERSION"
+
+name=AWS.DevOps
+echo $name
+echo The name is $name
